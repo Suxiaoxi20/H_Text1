6ee04617e6886cc8e1a4e9d1c9a480e20952f1e8
Dolrey <111716809+Dolrey@users.noreply.github.com>
2022-08-22 22:30:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -41,12 +41,11 @@ def get_birthday():
     next = next.replace(year=next.year + 1)
   return "距离小宝贝的生日还有"+str((next - today).days)+"天"
  
- def get_bigbirthday():
+def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return "距离大宝贝的生日还有"+str((next - today).days)+"天"
-
+  return "距离小宝贝的生日还有"+str((next - today).days)+"天"
 
 
 def get_words():
