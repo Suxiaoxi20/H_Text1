d18c40ffa009f954ccd4ee82a6a2f630d79f1763
Suxiaoxi20 <111843492+Suxiaoxi20@users.noreply.github.com>
2022-08-23 17:12:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_count():
   return delta.days
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d") 
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return "距离小宝贝的生日还有"+str((next - today).days)+"天"
