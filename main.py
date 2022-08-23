91e8d36402f660e03160440357e639eff9e4f4b6
Dolrey <111716809+Dolrey@users.noreply.github.com>
2022-08-23 22:37:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ data = {"t_today":{"value":t_today,"color":get_random_color()},
         "love_days":{"value":get_count(),"color":get_random_color()},
         "birthday_left":{"value":get_birthday(),"color":get_random_color()},
         "words":{"value":get_words(), "color":get_random_color()},
-        "shi":{"value":get_shi(), "color":get_random_color()},
+        "shi":{"value":get_shi(), "color":get_random_color()}}
       
 res = wm.send_template(user_id, template_id, data)
 print(res)
